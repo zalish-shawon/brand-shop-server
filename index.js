fc0d50d65ab06d62601b93129199f37c48095dee
zalish-shawon <zalish.shawon@gmail.com>
2023-10-21 23:18:48 +0600

    updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    //  await client.connect();
+     await client.connect();
     const productCollection = client.db("productsDB").collection("products");
     const myCartCollection = client.db("productsDB").collection("myCart");
 
