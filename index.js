5c44a9fa8bd9afd0f843b1f2830fb95372f5be9b
zalish-shawon <zalish.shawon@gmail.com>
2024-01-03 15:24:59 +0600

    404 error fixed

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port= process.env.PORT || 5000
 // brandShop
 // tuOPFNSBa2lPWhQD
 
-
+app.use(express.json())
+app.use(cors());
 
 const uri = "mongodb+srv://brandShop:tuOPFNSBa2lPWhQD@cluster0.hrjn1tt.mongodb.net/?retryWrites=true&w=majority";
 
@@ -24,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     await client.connect();
+    //  await client.connect();
     const productCollection = client.db("productsDB").collection("products");
     const myCartCollection = client.db("productsDB").collection("myCart");
 
@@ -135,7 +136,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -143,16 +144,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-app.use(express.json())
-
-
-app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Brand shop server is running")
